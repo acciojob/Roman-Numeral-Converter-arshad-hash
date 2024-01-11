9f41c13989bc7512b2e6e099bcf17c24346e8b11
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2024-01-12 00:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['IV', 4],
-    7: ['III', 3],
-    8: ['II', 2],
-    9: ['I', 1]
-  };
+  ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        ths = ["", "M", "MM", "MMM"]
 
-  let result = '';
-
-  for (let key in obj) {
-    // Access the values using obj[key]
-    let [roman, value] = obj[key];
-
-    // Calculate the number of times the current value goes into num
-    let count = Math.floor(num / value);
-
-    // Append the Roman numeral to the result
-    result += roman.repeat(count);
-
-    // Update num by subtracting the value * count
-    num -= value * count;
-  }
-
-  return result;
+        return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10]
 }
 
 // Test the function with an example (e.g., 36)
